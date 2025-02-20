632289349dbe3e7a5fd94dfb8b6df0162ee1337a
jarji <100996174+jarjia@users.noreply.github.com>
2025-02-20 22:46:40 +0400

    fix apollo

--- src/graphql/apollo.ts
+++ src/graphql/apollo.ts
@@ -2,9 +2,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: import.meta.env.VITE_PUBLIC_API_URL + "/graphql" }),
-  cache: new InMemoryCache({
-    addTypename: false,
-  }),
+  cache: new InMemoryCache(),
 });
 
 export default client;
